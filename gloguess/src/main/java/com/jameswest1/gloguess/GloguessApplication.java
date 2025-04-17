98cec90201351e3dfb4058df530e60d620855cf9
James West <jameswwest20@gmail.com>
2025-04-17 23:04:06 +1000

    initial

---
+++ gloguess/src/main/java/com/jameswest1/gloguess/GloguessApplication.java
@@ -0,0 +1,13 @@
+package com.jameswest1.gloguess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloguessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloguessApplication.class, args);
+	}
+
+}
---
+++ gloguess/src/test/java/com/jameswest1/gloguess/GloguessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameswest1.gloguess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloguessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
